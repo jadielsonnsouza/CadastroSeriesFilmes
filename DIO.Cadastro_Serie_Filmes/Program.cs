2051ea3294acc4bc1a0b6827afa011c3397e8fa3
jadielsonnsouza <jadielsonnsouza@gmail.com>
2021-05-25 21:19:24 -0300

    Criação do projeto em console de cadastro de serie e filme em C#

---
+++ DIO.Cadastro_Serie_Filmes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DIO.Cadastro_Serie_Filmes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
